a142b919bebe45c43d8f97dce9552ba219acb477
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-02 22:32:40 +0100

    Created form for deck creation

--- app/create/manual/page.tsx
+++ app/create/manual/page.tsx
@@ -1,8 +1,35 @@
+'use client'
+
+import { useState } from "react"
+import useCreateDeck from "@/hooks/filesystem/deck/useCreateDeck";
+
+// Compontents
 import Navbar from "@/components/UI/functional/Navbar"
 
+interface DeckData {
+    name: string;
+    description: string;
+}
+
 const createManual: React.FC = () => {
+    const [deckData, setDeckData] = useState<DeckData>({ name: '', description: '' })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.currentTarget;
+
+        setDeckData((prev) => ({
+            ...prev,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        useCreateDeck(deckData)
+    }
+
     return (
-        <main className="w-full">
+        <main className="w-full flex flex-col">
             <Navbar back />
 
             <section className="p-[10px] h-full flex flex-col">
@@ -14,9 +41,33 @@ const createManual: React.FC = () => {
                     </p>
                 </section>
 
-                <section>
+                <form className="h-full w-full flex flex-col gap-y-2 items-center justify-center" onSubmit={handleSubmit}>
+                    <section className="flex flex-col w-full md:w-2/3 gap-y-1">
+                        <section>
+                            <span className="text-sm text-light">Deck name</span>
+                            <span className="text-red-600 opacity-75">
+                                &nbsp;*
+                            </span>
+                        </section>
+                        <input className="styled-input" name="name" value={deckData.name} onChange={handleChange} required></input>
+                    </section>
 
-                </section>
+                    <section className="flex flex-col w-full md:w-2/3 gap-y-1">
+                        <span className="text-sm text-light">Deck description</span>
+                        <textarea className="styled-input" rows={10} value={deckData.description} name="description" onChange={handleChange}></textarea>
+                    </section>
+
+                    <section className="w-full md:w-2/3 text-ultralight flex text-xs">
+                        <span className="text-red-600 opacity-75">
+                            *
+                        </span>
+                        <span>Required</span>
+                    </section>
+
+                    <section className="w-full md:w-2/3">
+                        <button type="submit" className="styled-button">Create</button>
+                    </section>
+                </form>
             </section>
         </main>
     )
--- hooks/filesystem/deck/useCreateDeck.ts
+++ hooks/filesystem/deck/useCreateDeck.ts
@@ -2,3 +2,9 @@ interface Deck {
     name: string;
     description: string;
 }
+
+const useCreateDeck = ({ name, description }: Deck) => {
+    console.log(name, description)
+}
+
+export default useCreateDeck
