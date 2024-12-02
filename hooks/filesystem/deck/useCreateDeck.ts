69ad8232f53602edc6fba4b4abdfa662eb7b432b
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-02 23:06:24 +0100

    Created basic file creation The file created is NOT a valid JSON and just a template

--- hooks/filesystem/deck/useCreateDeck.ts
+++ hooks/filesystem/deck/useCreateDeck.ts
@@ -1,10 +1,14 @@
+import { create, BaseDirectory } from "@tauri-apps/plugin-fs"
+
 interface Deck {
     name: string;
     description: string;
 }
 
-const useCreateDeck = ({ name, description }: Deck) => {
-    console.log(name, description)
+const useCreateDeck = async ({ name, description }: Deck) => {
+    const file = await create(`${name}.json`, { baseDir: BaseDirectory.AppLocalData });
+    await file.write(new TextEncoder().encode(description));
+    await file.close();
 }
 
 export default useCreateDeck
