3b7330682db12d9c56780bcf0378910af2cf88a6
Eben Vranken <92394428+eben-vranken@users.noreply.github.com>
2024-12-02 23:57:37 +0100

    Added back link to 404 not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,12 +3,12 @@ import Navbar from "@/components/UI/functional/Navbar";
 const Page: React.FC = () => {
     return (
         <section className="w-full">
-            <Navbar pageTitle="Page Not Found" />
+            <Navbar back pageTitle="404" />
 
             <section className="p-[10px] h-full flex flex-col">
                 {/* Welcome message */}
                 <section className="mb-2">
-                    <h2 className="text-responsive-md font-semibold">404 Not found</h2>
+                    <h2 className="text-responsive-md font-semibold">Page Not found</h2>
                     <p className="text-responsive-sm text-light">
                         How did you end up here?
                     </p>
